2420d353a7f79bcff0e40cad1590bc0737c3d308
Ryan Leung <ryanleung1004@gmail.com>
2021-11-07 01:15:18 -0700

    Add skeleton files and upgrade gradle

--- app/src/main/java/com/bcit/pomodoro_scheduler/CalendarActivity.java
+++ app/src/main/java/com/bcit/pomodoro_scheduler/CalendarActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-/**
- * This is where the year view will go.
- */
-public class MainActivity extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calendar);
     }
 }
