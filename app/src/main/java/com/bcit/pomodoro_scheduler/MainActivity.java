e2572de34dc45e9974471ee22f6b9b9a0c304e6c
Shagun <54307245+Ishika18@users.noreply.github.com>
2021-11-18 09:12:30 -0800

    google sign in

--- app/src/main/java/com/bcit/pomodoro_scheduler/MainActivity.java
+++ app/src/main/java/com/bcit/pomodoro_scheduler/MainActivity.java
@@ -1,31 +1,120 @@
 package com.bcit.pomodoro_scheduler;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.bcit.pomodoro_scheduler.fragments.MonthFragment;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
-import java.time.Year;
+import java.time.YearMonth;
 
 /**
- * This is where the year view will go.
+ * This is where the login logic will go.
  */
 public class MainActivity extends AppCompatActivity {
+
+    SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
+    String GOOGLE_ACCOUNT = "googleAccount";
+    int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TO TEST MONTHLY VIEW
-        // goToMonthlyView(Year.of(2021));
+        setUpGoogleSign();
+        checkForExistingUser();
+        setUpSignInBtn();
     }
 
-    public void goToMonthlyView(Year year){
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragmentContainerView_main, MonthFragment.newInstance(year));
-        ft.commit();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Error", "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    public void setUpSignInBtn() {
+        // Set the dimensions of the sign-in button.
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    public void setUpGoogleSign() {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    public void checkForExistingUser() {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            updateUI(account);
+        }
+    }
+
+    public void updateUI(GoogleSignInAccount account) {
+        if (account == null) {
+            Log.w("LogIn", "Didn't Work");
+            return;
+        }
+
+        // for now just go to monthly view
+        Intent intent = new Intent(this, CalendarActivity.class);
+        intent.putExtra(GOOGLE_ACCOUNT, account);
+        startActivity(intent);
+        Log.w("LogIn", "Worked " + account.getEmail());
     }
 }
