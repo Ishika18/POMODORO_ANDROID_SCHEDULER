faefb41cfcf2f1c0f2a4a0500a5f5e7b6d490632
Shagun <54307245+Ishika18@users.noreply.github.com>
2021-11-27 17:22:43 -0800

    add or update schedule

--- app/src/main/java/com/bcit/pomodoro_scheduler/firebase/FirebaseDB.java
+++ app/src/main/java/com/bcit/pomodoro_scheduler/firebase/FirebaseDB.java
@@ -14,6 +14,8 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -135,6 +137,59 @@ public class FirebaseDB {
                 Log.d("DELETE_COMMITMENT_ERROR", e.getMessage());
             }
         });
+    }
+
+    public void addOrUpdateSchedule(
+            String userEmail, HashMap<LocalDate,
+            ArrayList<com.bcit.pomodoro_scheduler.model.Task>> schedule
+    ) {
+        for (Map.Entry<LocalDate, ArrayList<com.bcit.pomodoro_scheduler.model.Task>>
+                entry : schedule.entrySet()) {
+            db.collection("schedules").document(userEmail)
+                    .set(createScheduleObject(entry.getKey(), entry.getValue()))
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Log.d("Worked", "schedule added");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("Error", e.getLocalizedMessage());
+                        }
+                    });
+        }
+    }
+
+    public HashMap<String, Object> createScheduleObject(
+            LocalDate key, ArrayList<com.bcit.pomodoro_scheduler.model.Task> tasks
+    ) {
+        HashMap<String, Object> scheduleObject = new HashMap<>();
+        scheduleObject.put(key.toString(), createTasksListObject(tasks));
+        return scheduleObject;
+    }
+
+    public ArrayList<Object> createTasksListObject (
+            ArrayList<com.bcit.pomodoro_scheduler.model.Task> tasks
+    ) {
+        ArrayList<Object> taskObjects = new ArrayList<>();
+        for (com.bcit.pomodoro_scheduler.model.Task task: tasks) {
+            taskObjects.add(createTaskObject(task));
+        }
+        return taskObjects;
+    }
+
+    public HashMap<String, Object> createTaskObject (
+            com.bcit.pomodoro_scheduler.model.Task task
+    ) {
+        HashMap<String, Object> taskObject = new HashMap<>();
+        taskObject.put("id", task.getID());
+        taskObject.put("name", task.getName());
+        taskObject.put("startTime", task.getEndTime());
+        taskObject.put("endTime", task.getEndTime());
+        taskObject.put("type", task.getType().name());
+        return taskObject;
     }
 
     public FirebaseFirestore getDb() {
